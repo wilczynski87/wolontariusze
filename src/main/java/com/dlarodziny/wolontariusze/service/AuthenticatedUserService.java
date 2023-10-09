ce620e8d0831f5250fc9b99553cb923a7d211282
Karol <wilczynski87@gmail.com>
2023-10-09 12:42:11 +0200

    quick fix

--- src/main/java/com/dlarodziny/wolontariusze/service/AuthenticatedUserService.java
+++ src/main/java/com/dlarodziny/wolontariusze/service/AuthenticatedUserService.java
@@ -23,31 +23,4 @@ public class AuthenticatedUserService implements ReactiveUserDetailsService {
         return volunteerRepo.findByUsername(username).map(AuthenticatedUser::new);
     }
     
-}package com.dlarodziny.wolontariusze.service;
-
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
-import com.dlarodziny.wolontariusze.model.AuthenticatedUser;
-import com.dlarodziny.wolontariusze.repository.VolunteerRepo;
-
-import reactor.core.publisher.Mono;
-
-@Component
-public class AuthenticatedUserService implements ReactiveUserDetailsService {
-
-    private final VolunteerRepo volunteerRepo;
-
-    AuthenticatedUserService(VolunteerRepo volunteerRepo) {
-        this.volunteerRepo = volunteerRepo;
-    }
-
-    @Override
-    public Mono<UserDetails> findByUsername(String username) {
-        return volunteerRepo.findByUsername(username).map(AuthenticatedUser::new);
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'findByUsername'");
-    }
-    
 }

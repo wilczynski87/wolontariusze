4c915e019f4201dd50f91400fa96bbf6436e6839
Karol <wilczynski87@gmail.com>
2023-11-14 11:49:38 +0100

    attitude update

--- src/main/java/com/dlarodziny/wolontariusze/model/Attitude.java
+++ src/main/java/com/dlarodziny/wolontariusze/model/Attitude.java
@@ -1,18 +1,38 @@
 package com.dlarodziny.wolontariusze.model;
 
 public enum Attitude {
-    ZIMNY("Zimny"),
-    CHLODNY("Chłodny"),
-    CIEPLY("Ciepły"),
-    GORACY("Gorący");
+    ZIMNY("Zimny", 0),
+    CHLODNY("Chłodny", 1),
+    CIEPLY("Ciepły", 2),
+    GORACY("Gorący", 3),
+    AKTYWNY("Aktywny", 4),
+    UTRACONY("Utracony", 5),
+    NIEPODANO("nie podano", 6)
+    ;
 
     private final String attitude;
+    private final int number;
 
-    Attitude(String attitude) {
+    Attitude(String attitude, int number) {
         this.attitude = attitude;
+        this.number = number;
     }
 
     public String getAttitude() {
         return this.attitude;
+    }
+    public int getNumber() {
+        return this.number;
+    }
+
+    public static Attitude findByValue(String value) {
+        Attitude result = null;
+        for (Attitude attitude : values()) {
+            if (value.toLowerCase().contains(attitude.getAttitude().toLowerCase())) {
+                result = attitude;
+                break;
+            }
+        }
+        return result;
     }
 }

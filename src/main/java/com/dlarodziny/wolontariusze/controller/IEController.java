870a874c813b92be8c7d4222c52bd1a7fa131b4a
Karol <wilczynski87@gmail.com>
2023-11-21 14:18:44 +0100

    added export contacts

--- src/main/java/com/dlarodziny/wolontariusze/controller/IEController.java
+++ src/main/java/com/dlarodziny/wolontariusze/controller/IEController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.result.view.Rendering;
 
+import com.dlarodziny.wolontariusze.model.Contact;
 import com.dlarodziny.wolontariusze.model.Volunteer;
 import com.dlarodziny.wolontariusze.model.VolunteerDetails;
+import com.dlarodziny.wolontariusze.service.ContactService;
 import com.dlarodziny.wolontariusze.service.IEService;
 import com.dlarodziny.wolontariusze.service.VolunteerDetailsService;
 import com.dlarodziny.wolontariusze.service.VolunteerService;
@@ -32,11 +34,13 @@ public class IEController {
     private final IEService ieService;
     private final VolunteerService volunteerService;
     private final VolunteerDetailsService volunteerDetailsService;
+    private final ContactService contactService;
 
-    public IEController(IEService ieService, VolunteerService volunteerService, VolunteerDetailsService volunteerDetailsService) {
+    public IEController(IEService ieService, VolunteerService volunteerService, VolunteerDetailsService volunteerDetailsService, ContactService contactService) {
         this.ieService = ieService;
         this.volunteerService = volunteerService;
         this.volunteerDetailsService = volunteerDetailsService;
+        this.contactService = contactService;
     }
 
     @GetMapping("/getVolunteers")
@@ -78,4 +82,30 @@ public class IEController {
 
         return volunteerDetailsService.saveVolunteerDetails(volunteerDetails);
     }
+
+    // Export data
+    @ResponseBody
+    @PostMapping("/exportVolunteers")
+    public Flux<Volunteer> exportVolunteers(final Model model, Authentication auth) {
+        log.info("\n\nexportVolunteers dotknięte!!!\n");
+
+        return volunteerService.getAllVlounteers();
+    }
+
+    @ResponseBody
+    @PostMapping("/exportVolunteerDetails")
+    public Flux<VolunteerDetails> exportVolunteerDetails(final Model model, Authentication auth) {
+        log.info("\n\nexportVolunteerDetails dotknięte!!!\n");
+
+        return volunteerDetailsService.getAllVolunteerDetails();
+    }
+
+    @ResponseBody
+    @PostMapping("/exportContacts")
+    public Flux<Contact> exportContacts(final Model model, Authentication auth) {
+        log.info("\n\nexportContacts dotknięte!!!\n");
+
+        return contactService.getAllContacts();
+    }
+
 }
